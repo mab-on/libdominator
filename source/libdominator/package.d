1cc91290d47d53fc87d8e62caddeea7cde202440
Martin Brzenska <martin@mab-on.net>
2016-12-11 20:31:01 +0100

    Removed submodule libdominator.Output

--- source/libdominator/package.d
+++ source/libdominator/package.d
@@ -2,8 +2,8 @@
  * Copyright:
  * (C) 2016 Martin Brzenska
  *
- * License: 
- * Distributed under the terms of the MIT license. 
+ * License:
+ * Distributed under the terms of the MIT license.
  * Consult the provided LICENSE.md file for details
  */
 module libdominator;
@@ -12,4 +12,3 @@ public import libdominator.Attribute;
 public import libdominator.Dominator;
 public import libdominator.Node;
 public import libdominator.Filter;
-public import libdominator.Output;
